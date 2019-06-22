423d2092d55cbbf70c26505ab4bb7734027d7655
CamCam <CamCam@Determination>
2019-06-22 01:13:51 -0700

    Partial cleanup of the script, setting a basic pattern for how commenting and organization of the script will be.

--- EOUSful Installation.bat
+++ EOUSful Installation.bat
@@ -1,27 +1,38 @@
 @ECHO OFF
 
+rem -= Convenient Variables =-
+rem DESCRIPTION: These are the long and/or constantly used variables where I would like to save space.
+rem ====================================================================================================================
+
 SET EXEName=DesktopOK_x64.exe
 SET EXEPath=C:\EaseOfUse\DesktopOK_x64\DesktopOK_x64.exe
 SET DRIVEPath=%~d0%
 
-echo -----------------------------------------------------------------------------
+rem -= Convenient Variables End =-
+rem --------------------------------------------------------------------------------------------------------------------
 
-echo Now installing the Ease Of Use Suite, designed for conference center laptops.
+echo + ----------------------------------------------------------------------------- +
 
-echo -----------------------------------------------------------------------------
-echo.
+echo + Now installing the Ease Of Use Suite, designed for conference center laptops. +
 
-rem //----------------------------------------------------------------------------
+echo + ----------------------------------------------------------------------------- +
 
+rem -= Administrator Privileges =-
+rem DESCRIPTION: The Gist of this section: checks for administrator privileges, as they are required for this script.
+rem ====================================================================================================================
+
+rem COMMENT: This subsection checks for admin privileges, and if not available moves to the UACPrompt subsection;
+rem - otherwise, it simply moves to gotAdmin for already having the correct privileges.
 echo Checking for required permissions...
-echo.
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
 if "%errorlevel%" NEQ '0' (
 	echo Missing required permissions, requesting administrative privileges...
 	goto UACPrompt
 ) else ( goto gotAdmin )
 
+rem COMMENT: This subsection is what elevates the script, but opening a new commandline with admin privileges and
+rem - re-executing this script from that new commandline instance. Once the new instance of the script is run, it then
+rem - exits out of the initial, non-admin privileges, instance.
 :UACPrompt
 	echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 	set params = %*:"=""
@@ -30,13 +41,15 @@ if "%errorlevel%" NEQ '0' (
     	del "%temp%\getadmin.vbs"
 	exit /b
 
+rem COMMENT: This subsection is very simple: it lets the user know that they already had the correct privileges and
+rem - continues on with the rest of the script!
 :gotAdmin
 	echo Correct permissions available!
 	rem pushd "%CD%"
 	rem cd /D "%dp0"
 
-rem //----------------------------------------------------------------------------
-
+rem -= Administrator Privileges End=-
+rem --------------------------------------------------------------------------------------------------------------------
 echo Step 1 - Copy over all neccessary files from USB drive.
 GOTO validateShortcutPaths
 
