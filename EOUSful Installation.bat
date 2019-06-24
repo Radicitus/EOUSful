c18a4b7b2b1791953dccf1f0054e9687061248db
CamCam <courage@camcam.cam>
2019-06-24 12:33:24 -0700

    Testing a more robust elevation function  pt.2

--- EOUSful Installation.bat
+++ EOUSful Installation.bat
@@ -22,41 +22,7 @@ rem ============================================================================
 rem DESCRIPTION: The Gist of this section: checks for administrator privileges, as they are required for this script.
 rem ====================================================================================================================
 
-setlocal DisableDelayedExpansion
-set cmdInvoke=1
-set winSysFolder=System32
-for %%k in (%0) do set batchName=%%~nk
-set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
-setlocal EnableDelayedExpansion
-
-NET FILE 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto gotPriv ) else (
-    echo Missing required permissions, attempting to elevate...
-    goto getPriv )
-
-:getPriv
-if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPriv)
-ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
-ECHO args = "ELEV " >> "%vbsGetPrivileges%"
-ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
-ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
-ECHO Next >> "%vbsGetPrivileges%"
-if '%cmdInvoke%'=='1' goto InvokeCmd
-ECHO UAC.ShellExecute "!INSTALLATIONPATH\EOUSful Installation.bat!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-goto execElevation
-
-:invokeCmd
-ECHO args = "/c """ + "!INSTALLATIONPATH\EOUSful Installation.bat!" + """ " + args >> "%vbsGetPrivileges%"
-ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-
-:execElevation
-"%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
-exit /B
-
-:gotPriv
-setlocal & cd /d %~dp0
-if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
-echo Necessary permissions available!
+"
 
 rem -= Administrator Privileges End=-
 rem --------------------------------------------------------------------------------------------------------------------
