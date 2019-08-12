12bb220428402b4a9f97a38147c8afd7c0d9d386
Radicitus@Firelight <crsherry@uci.edu>
2019-08-12 16:58:04 -0700

    Modified createShortcut to make syntax make more sense when creating a shortcut.

--- Scripts/Utilities/createShortcut.ps1
+++ Scripts/Utilities/createShortcut.ps1
@@ -1,8 +1,9 @@
-﻿param($sourceExecutable,$destinationPath)
+﻿param([Parameter(Mandatory=$True,Position = 0)] $sourceExecutable, [Parameter(Mandatory=$True,ValueFromRemainingArguments=$True,Position = 1)] $destinationPaths)
 
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($destinationPath)
-$Shortcut.TargetPath = $sourceExecutable
-$Shortcut.Save()
-
-Write-Host '  Shortcut created!' -ForegroundColor DarkCyan
+ForEach ($dPath in $destinationPaths) {
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($dPath)
+    $Shortcut.TargetPath = $sourceExecutable
+    $Shortcut.Save()
+    Write-Host '  Shortcut for ...'+  + ' placed at ...' + $dPath.substring($dPath.length - 5) + ' created!' -ForegroundColor DarkCyan
+}
